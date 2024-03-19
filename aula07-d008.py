7d8781a0716a5d57dd324c728745f5d38714f24e
Lylian Martins <lylimartins@yahoo.com>
2024-03-19 19:57:53 -0300

    atualização

--- aula07-d008.py
+++ aula07-d008.py
@@ -1,5 +1,5 @@
-v1=(float(input('Digite o valor em metros que deseja converter: ')))
+medida=(float(input('Digite o valor em metros que deseja converter: ')))
 cent= v1*100
 mili= v1*1000
-print('{} metros é {} em centímetros'.format(v1, cent))
-print('{} metros é {} em milimetros'.format(v1, mili))
+print('{} metros é {} em centímetros'.format(medida, cent))
+print('{} metros é {} em milimetros'.format(medida, mili))
--- aula07-d010.py
+++ aula07-d010.py
@@ -1,7 +1,7 @@
 print('CONVERSOR DE MOEDAS')
 print()
 
-real=int(input('Digite o valor que deseja converter: R$ '))
+real=float(input('Digite o valor que deseja converter: R$ '))
 conversao_dolar = real / 3.27
 print('Com R$ {:.2f} reais você tem U$ {:.2f} dólares '.format(real,conversao_dolar))
 
