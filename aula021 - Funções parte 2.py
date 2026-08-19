d1b70ff5147764375b37f1b8d97ce1a7f041fa75
Lylian Martins <lylimartins@yahoo.com>
2026-08-19 11:04:26 -0300

    Atualização do Dia

---
+++ aula021 - Funções parte 2.py
@@ -0,0 +1,10 @@
+'''Funções parte 2:
+
+- Interactive Help
+- docstrings
+- rgumentos opcionais
+- espoco de variáveis
+- retorno de resultados '''
+
+#INTERECTIVE HELP
+#help() no python console é um comando que quando vc digita pode procurar qualquer funcao e ele te explica do que se trata
